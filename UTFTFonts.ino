115cd52e5a7ba3ef26d384a75917aca2226e488a
the lonely programmer <rahulkhanna24.06@gmail.com>
2019-04-02 01:26:23 +0530

    Update UTFTFonts.ino

--- UTFTFonts.ino
+++ UTFTFonts.ino
@@ -15,21 +15,29 @@ extern uint8_t SevenSegNumFont[]; // Set the pins to the correct ones for your d
 // CTE TFT LCD/SD Shield for Arduino Mega      : <display model="">,38,39,40,41
 //
 // Remember to change the model parameter to suit your display module!
-UTFT myGLCD(ITDB32S,38,39,40,41);</display></display></display></display></p><p>void setup()
+UTFT myGLCD(ITDB32S,38,39,40,41);
+void setup()
 {
-  myGLCD.InitLCD();</p><p>  myGLCD.clrScr();
-}</p><p>void loop()
+  myGLCD.InitLCD()
+  myGLCD.clrScr();
+}
+
+void loop()
 {
   myGLCD.setColor(0, 255, 0);
-  myGLCD.setBackColor(0, 0, 0);</p><p>  myGLCD.setFont(BigFont);
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setFont(BigFont);
   myGLCD.print(" !\"#$%&'()*+,-./", CENTER, 0);
   myGLCD.print("0123456789:;<=>?", CENTER, 16);
   myGLCD.print("@ABCDEFGHIJKLMNO", CENTER, 32);
   myGLCD.print("PQRSTUVWXYZ[\\]^_", CENTER, 48);
   myGLCD.print("`abcdefghijklmno", CENTER, 64);
-  myGLCD.print("pqrstuvwxyz{|}~ ", CENTER, 80);</p><p>  myGLCD.setFont(SmallFont);
+  myGLCD.print("pqrstuvwxyz{|}~ ", CENTER, 80);
+  myGLCD.setFont(SmallFont);
   myGLCD.print(" !\"#$%&'()*+,-./0123456789:;<=>?", CENTER, 120);
   myGLCD.print("@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_", CENTER, 132);
-  myGLCD.print("`abcdefghijklmnopqrstuvwxyz{|}~ ", CENTER, 144);</p><p>  myGLCD.setFont(SevenSegNumFont);
-  myGLCD.print("0123456789", CENTER, 190);</p><p>  while(1) {};
+  myGLCD.print("`abcdefghijklmnopqrstuvwxyz{|}~ ", CENTER, 144);
+  myGLCD.setFont(SevenSegNumFont);
+  myGLCD.print("0123456789", CENTER, 190);
+  while(1) {};
 }
